6392ef6e823d12519ed8a5be724f646c2bc87f0d
Julien Constant <julienconstant190@gmail.com>
2025-02-16 00:50:46 +0100

    scripts: add zip script

--- .scripts/src/constants.ts
+++ .scripts/src/constants.ts
@@ -3,6 +3,7 @@ import path from 'node:path';
 export const MODS_DIRECTORY = path.resolve('..', '..', '..', 'mods');
 export const DEFAULT_TEXTURES_DIRECTORY = path.resolve('..', '.default');
 export const FAITHFUL_TEXTURES_DIRECTORY = path.resolve('..', 'assets');
+export const ROOT_DIRECTORY = path.resolve('..');
 export const RESOURCE_PACK_DIRECTORY = path.resolve('..');
 export const CACHE_FILE = path.resolve('cache.json');
 export const PACK_MCMETA = path.resolve('..', 'pack.mcmeta');
